9e2f7525055b925a3625c76e6b121de755f1049f
Lnellr <meow.daria@inbox.ru>
2024-04-07 22:47:08 +0300

    task 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//The first
 const convertToObject = (content) => {
   // отделили шапку от содержимого
   const [keys, ...apps] = content.trim().split('\n');
@@ -78,10 +79,89 @@ const tableParsing = (content) => {
   console.log(`Top owner: ${mono}`);
 };
 
-// task 2
-const candidateAssessment = (/* content */) => {
 
+
+//The second
+const Kukish = (content) => {
+  const contentSplit = content.split('\n');
+  return contentSplit;
 };
+
+const stackcount = (content) => {
+  const contentSplit = content.split('\n');
+  const stack = ['React', 'Angular', 'Vue.js', 'JQuery', 'Backbone.js', 'Node.js', 'Ember.js', 'Meteor'];
+  const lowercontentSplit = contentSplit[5].toLowerCase();
+  let res = 0;
+  const lowerstack = stack.map((item) => item.toLowerCase());
+  for (let i = 0; i < stack.length; i += 1) {
+    if (lowercontentSplit.includes(lowerstack[i])) {
+      res += 1;
+    }
+  }
+  return res;
+};
+
+
+const getNickName = (socialString, socialNetwork) => {
+  const socials = socialString.split(', ');
+  for (let i = 0; i < socials.length; i += 1) {
+    const parts = socials[i].split(':/');
+    const username = parts[parts.length - 1].split('/').pop();
+    if (parts[0].includes(socialNetwork)) {
+      return username;
+    }
+  }
+  return 'Никнейм не найден';
+};
+
+
+const calculateExp = (startDate, endDate) => {
+  const start = new Date(startDate.split('.').reverse().join('-'));
+  const end = new Date(endDate.split('.').reverse().join('-'));
+
+  let years = end.getFullYear() - start.getFullYear();
+  let months = end.getMonth() - start.getMonth();
+
+  if (months < 0) {
+    years -= 1;
+    months += 12;
+  }
+
+  return `${years} years ${months} months`;
+};
+
+
+const findEducation = (edStr) => {
+  const parts = edStr.split(';');
+  let result = '';
+  const EdPlaces = parts.map((part) => {
+    const trimmedPart = part.trim();
+    return trimmedPart.split(',')[0];
+  });
+  if (EdPlaces[0].toLowerCase().includes('education:')) {
+    EdPlaces[0] = EdPlaces[0].replace(/education:/gi, '').trim();
+  }
+  const sortedEd = EdPlaces.sort();
+  result = sortedEd.join(', ');
+  return result;
+};
+
+// Task 2
+const candidateAssessment = (content) => {
+  const Split = Kukish(content);
+  // task2.1
+  console.log(`Job seeker: ${Split[0]}, ${Split[1]}`);
+  // task2.2
+  console.log(`Required stack: ${stackcount(content)}`);
+  // task2.3
+  console.log(`GitHub nickname: ${getNickName(Split.at(4), 'github')}`);
+  // task2.4
+  console.log(`Experience: ${calculateExp('01.01.2015', '05.06.2022')}`);
+  // task2.5
+  console.log(`Education: ${findEducation(Split.at(7))}`);
+};
+
+//The third
 
 // task 3
 const actorRating = (/* content */) => {
